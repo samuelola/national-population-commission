84742d948a85ef00706509a1932388f9c13f380f
iamsamuel <sammyola122@gmail.com>
2022-04-01 01:08:54 +0100

    users states lga ward citizen migration files

--- database/migrations/2022_04_01_000130_create_citizens_table.php
+++ database/migrations/2022_04_01_000130_create_citizens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCitizensTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('citizens', function (Blueprint $table) {
             $table->id();
+            $table->integer('ward_id');
             $table->string('name');
-            $table->string('email')->unique();
-            
-            $table->string('password');
-           
+
+            $table->string('gender');
+            $table->string('address');
+            $table->string('phone');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('citizens');
     }
 }
